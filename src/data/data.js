11cc7a5458f2dc01db42c25b9be14f06963db508
MORGANJASON <420morganjason@gmail.com>
2023-04-29 11:55:26 +0300

    completed lab

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,6 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+export function name1 () {"Liza"};
+export function city () {"New York"};
+export function image () {"https://i.imgur.com/mV8PQxj.gif"};
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
-
-export { name, city, image };
+// export { name, city, image };
